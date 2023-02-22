62f35bed1bd7b8008262cc21d7e917ad301a5daa
Yuvraj <sharma.yuvraj.00786@gmail.com>
2023-02-22 20:32:19 +0530

    SideWall topics and trending

--- src/Components/mainWall.jsx
+++ src/Components/mainWall.jsx
@@ -1,37 +1,73 @@
-import { Link, Paper, Typography } from "@mui/material";
-import { Box, Container, Stack } from "@mui/system";
+import { Container, Link, Paper, Tooltip, Typography } from "@mui/material";
+import { Box, Stack } from "@mui/system";
 import React, { useContext } from "react";
 import AppContext from "../Context/AppContext";
-import Sidebar from "./sidebar";
+// import Sidebar from "./sidebar";
+import "../styles/mainwall.css";
 
 function MainWall() {
-
-
   const Blogs = useContext(AppContext);
-  const { bposts, setBposts } = Blogs;
+  const { bposts } = Blogs;
 
   return (
-    <Box >
-      {
-        bposts.map((item, i) => {
-          return(
-            <React.Fragment key={i}>
-            <Box >
-                <Paper elevation={5} sx={{mt: 5, padding: '25px',  opacity: 0.8}}>
-                  <Stack spacing={2}>
-                    <Typography variant="subtitle2" align="center" sx={{color: 'var( --color-two )'}}> {item.title} </Typography>
-                    <Typography variant="h6" align="center" sx={{color: 'var( --color-one )'}}> {item.topic} </Typography>
-                    <Typography variant="body1" align="left"> {item.content} </Typography>
-                    <Link> {item.author}</Link>
-                  </Stack>
-                </Paper>
+    <Box>
+      {bposts.map((item, i) => {
+        return (
+          <React.Fragment key={i}>
+            <Box>
+              <Paper
+                elevation={5}
+                sx={{
+                  mt: 5,
+                  mb: 2,
+                  padding: "25px",
+                  backgroundColor: "#F5F5F5"
+                }}
+              >
+                <Stack spacing={2}>
+                  <Typography
+                    variant="subtitle2"
+                    align="center"
+                    sx={{ color: "var( --color-two )" }}
+                  >
+                    {" "}
+                    {item.title}{" "}
+                  </Typography>
+                  <Typography
+                    variant="h6"
+                    align="center"
+                    sx={{ color: "var( --color-two )" }}
+                  >
+                    {" "}
+                    {item.topic}{" "}
+                  </Typography>
+                  {item.image !== undefined && (
+                    <Container align="center">
+                      <Tooltip title="Image" placement="right">
+                        <img
+                          src={item.image}
+                          alt={item.title}
+                          loading="lazy"
+                          className="heroblogimagesize"
+                        />
+                      </Tooltip>
+                    </Container>
+                  )}
+                  <Typography variant="body1" align="left">
+                    {" "}
+                    {item.shortContent}{" "}
+                  </Typography>
+                  <Typography>
+                    <Link sx={{ color: "var( --color-one )", cursor: "pointer"}}> {item.author}</Link>
+                  </Typography>
+                </Stack>
+              </Paper>
             </Box>
-            </ React.Fragment>
-          )
-        })
-      }
+          </React.Fragment>
+        );
+      })}
     </Box>
-  )
+  );
 }
 
 export default MainWall;
--- src/Components/sidebar.jsx
+++ src/Components/sidebar.jsx
@@ -1,16 +1,101 @@
-import { Box, Paper } from '@mui/material';
-import React from 'react'
+import { Box, Paper, ToggleButton, ToggleButtonGroup, Divider, Stack, Typography } from "@mui/material";
+import React, { useContext, useState } from "react";
+import AppContext from "../Context/AppContext";
 
 function Sidebar() {
+  const InterestOptions = useContext(AppContext);
+  const { interestfields, bposts } = InterestOptions;
+
+  const shuffle = arr => [...arr].sort(() => Math.random() - 0.5);
+  const newList = shuffle(bposts);
+
+  const [formats, setFormats] = useState([]);
+
+  const handleFormat = (e, formats) => {
+    setFormats(formats);
+    console.log(formats);
+  };
+
   return (
     <React.Fragment>
-            <Box>
-                <Paper elevation={5} sx={{mt: 5, padding: '25px',  opacity: 0.9, backgroundColor: 'var( --color-two )'}}>
-                 
-                </Paper>
+      <Box>
+        <Paper
+          elevation={5}
+          sx={{
+            mt: 5,
+            padding: "25px",
+            backgroundColor: "var( --color-four )"
+          }}
+        >
+
+            <Box sx={{mt: '10px'}}>
+              <Typography variant="body1" align="center" sx={{textDecoration: 'underline', textDecorationColor: '#00ADB5', mb: '2px'}}>
+                Trending topics this week
+              </Typography>
             </Box>
-    </ React.Fragment>
-  )
+
+
+          <ToggleButtonGroup
+            value={formats}
+            onChange={handleFormat}
+            aria-label="Interest Options"
+            size="small"
+            sx={{ flexWrap: "wrap" }}
+          >
+            {interestfields.map((item) => {
+              return (
+                <ToggleButton
+                  key={item.interestId}
+                  value={item.topic}
+                  aria-label={item.topic}
+                  sx={{
+                    padding: "4px",
+                    width: "auto",
+                    flex: "none",
+                    "&.MuiToggleButtonGroup-grouped": {
+                      mx: 2,
+                      my: 1,
+                      border: "1px solid #00ADB5 !important",
+                      borderRadius: "3px 7px !important",
+                      color: "var( --color-one )"
+                    },
+                  }}
+                >
+                  {item.topic}
+                </ToggleButton>
+              );
+            })}
+          </ToggleButtonGroup>
+
+
+         <Divider sx={{marginTop: "15px"}}/>
+          
+          <Stack direction="column">
+            <Box sx={{mt: '10px'}}>
+            <Typography variant="body1" align="center" sx={{textDecoration: 'underline', textDecorationColor: '#00ADB5', mb: '2px'}}>
+              Trending blogs this week
+            </Typography>
+            </Box>
+            {
+              newList.map((item, i) => {
+                return(
+                 
+                    <Stack direction="column" key={i} sx={{mt: '10px', mb: '10px', cursor: 'pointer'}}>
+                    <Box sx={{ display: 'inline', border: '1px solid #00ADB5', p: '7px', borderRadius: '5px'}}>
+                        {i + 1}. {item.title}
+                      </Box>
+                      
+                    </Stack>
+                  
+                );
+              })
+            }
+          </Stack>
+         
+        </Paper>
+      </Box>
+    </React.Fragment>
+  );
 }
 
 export default Sidebar;
