f5be93dbd74f23f7f4da0bbea45cffd9928a729a
Yuvraj <sharma.yuvraj.00786@gmail.com>
2023-02-23 00:50:27 +0530

    change in home view

--- src/Components/blogView.jsx
+++ src/Components/blogView.jsx
@@ -1,72 +1,33 @@
-import { Container, Link, Paper, Tooltip, Typography } from "@mui/material";
-import { Box, Stack } from "@mui/system";
-import React, { useContext } from "react";
-import AppContext from "../Context/AppContext";
-// import Sidebar from "./sidebar";
-import "../styles/mainwall.css";
+import {
+  Box,
+  Container,
+  Link,
+  Paper,
+  Tooltip,
+  Typography,
+} from '@mui/material';
+import React, { useContext } from 'react';
+import AppContext from '../Context/AppContext';
+import Image1 from "../Assests/Images/image_1.jpg";
 
 function BlogView() {
   const Blogs = useContext(AppContext);
   const { bposts } = Blogs;
 
   return (
-    <Box>
-      {bposts.map((item, i) => {
-        return (
-          <React.Fragment key={i}>
-            <Box>
-              <Paper
-                elevation={5}
-                sx={{
-                  mt: 5,
-                  mb: 2,
-                  padding: "25px",
-                  backgroundColor: "#F5F5F5"
-                }}
-              >
-                <Stack spacing={2}>
-                  <Typography
-                    variant="subtitle2"
-                    align="center"
-                    sx={{ color: "var( --color-two )" }}
-                  >
-                    {" "}
-                    {item.title}{" "}
-                  </Typography>
-                  <Typography
-                    variant="h6"
-                    align="center"
-                    sx={{ color: "var( --color-two )" }}
-                  >
-                    {" "}
-                    {item.topic}{" "}
-                  </Typography>
-                  {item.image !== undefined && (
-                    <Container align="center">
-                      <Tooltip title="Image" placement="right">
-                        <img
-                          src={item.image}
-                          alt={item.title}
-                          loading="lazy"
-                          className="heroblogimagesize"
-                        />
-                      </Tooltip>
-                    </Container>
-                  )}
-                  <Typography variant="body1" align="left">
-                    {" "}
-                    {item.shortContent}{" "}
-                  </Typography>
-                  <Typography>
-                    <Link sx={{ color: "var( --color-one )", cursor: "pointer"}}> {item.author}</Link>
-                  </Typography>
-                </Stack>
-              </Paper>
-            </Box>
-          </React.Fragment>
-        );
-      })}
-    </Box>
+    <Container>
+      <Box display={'flex'}>
+        <Box>
+          {' '}
+          <img
+            src={Image1}
+            // alt={item.title}
+            loading="lazy"
+          />
+        </Box>
+        <Box></Box>
+      </Box>
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import BlogPosts from './Context/blogPosts';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./Styles/index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import BlogPosts from "./Context/blogPosts";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
-    <BrowserRouter>
-      <BlogPosts>
-        <App />
-      </BlogPosts>
-    </BrowserRouter>
+  <BrowserRouter>
+    <BlogPosts>
+      <App />
+    </BlogPosts>
+  </BrowserRouter>
 );
